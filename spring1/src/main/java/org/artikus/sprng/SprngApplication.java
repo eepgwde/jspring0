1627f92ec304ab00fdcec2887e099cef2d457b7f
Walter Eaves <w.eaves@reply.de>
2021-12-16 15:18:41 +0100

    WEB JPA H2

---
+++ spring1/src/main/java/org/artikus/sprng/SprngApplication.java
@@ -0,0 +1,13 @@
+package org.artikus.sprng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngApplication.class, args);
+	}
+
+}
